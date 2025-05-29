f280b700dffcf7866acd4aaad29f613fa34d8b95
howardjr1105 <howardescerrato@gmail.com>
2025-05-29 10:59:15 -0600

    add map y header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
-import CountdownTimer from "./CountdownTimer";
+import CountdownTimer from "./components/CountdownTimer";
+import Header from "./components/Header";
+import Map from "./components/Map";
 
 function App() {
   const fechaObjetivo = "2025-09-01T15:00:00";
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <CountdownTimer targetDate={fechaObjetivo} />
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen flex items-center justify-center bg-white">
+        <CountdownTimer targetDate={fechaObjetivo} />
+      </div>
+      <Map />
+    </>
   );
 }
 
