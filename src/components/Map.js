e1850c4901d1fc19b8ef081c9ca6ee61975d95d4
howardjr1105 <howardescerrato@gmail.com>
2025-05-29 10:52:33 -0600

    creacion map.js

---
+++ src/components/Map.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div>
+      <h2>Ubicación del evento</h2>
+      <iframe
+        title="Mapa"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3897.0092192348443!2d-85.51691452642179!3d12.382314427526941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f73735c234fea1d%3A0xe34ba8662cdacadb!2sHassan%20Restaurante!5e0!3m2!1ses!2sni!4v1748481836598!5m2!1ses!2sni"
+        width="100%"
+        height="300"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Map;
