44c9a3399129f055e3075383c916a9ec22a79674
howardjr1105 <howardescerrato@gmail.com>
2025-05-29 11:24:45 -0600

    mejora Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Header = () => {
+  // Obtener el nombre del invitado desde la URL
+  const params = new URLSearchParams(window.location.search);
+  const guestName = params.get("invitado") || "Estimado Invitado";
+
   return (
     <header className="header">
+      <h2>Para, {guestName}!</h2>
       <h1>¡Nos Casamos! 💍</h1>
       <p>Alex & Doriam - 01 de septiembre de 2025</p>
     </header>
