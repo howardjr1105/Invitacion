a557650159a47086fa0d6b26f41295e798311b33
howardjr1105 <howardescerrato@gmail.com>
2025-05-29 10:51:35 -0600

    Creacion Hedaer

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>¡Nos Casamos! 💍</h1>
+      <p>Juan & María - 15 de septiembre de 2025</p>
+    </header>
+  );
+};
+
+export default Header;
